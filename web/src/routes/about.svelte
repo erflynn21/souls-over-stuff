cbf017e9d77e88218c7da680976d05e36f4567d0
Evan Flynn <erflynn21@gmail.com>
2021-03-03 14:34:38 +0800

    adds about page content

--- web/src/routes/about.svelte
+++ web/src/routes/about.svelte
@@ -1,7 +1,16 @@
 <svelte:head>
-	<title>About</title>
+    <title>About</title>
 </svelte:head>
 
-<h1>About this site</h1>
+<h1 class="text-center my-8 text-3xl font-shippori">About</h1>
 
-<p>This is the 'about' page. There's not much here.</p>
+<div class="content max-w-2xl mx-auto text-lg leading-8">
+    <p class="mb-6">
+        Souls Over Stuff is about prioritizing the right things in life, people
+        over stuff. It's about filling our lives with the relationships that
+        truly matter and deliberately getting rid of things that don't. It's
+        about finding joy, contentment, and purpose by living the way that we
+        were created to, not the way the world tells us to.
+    </p>
+    <p>Welcome to Souls Over Stuff.</p>
+</div>
