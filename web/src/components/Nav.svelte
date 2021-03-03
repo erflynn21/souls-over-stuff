0a762b66a0d56af22e082e644732585f2b881908
Evan Flynn <erflynn21@gmail.com>
2021-03-03 14:14:23 +0800

    changes nav logo

--- web/src/components/Nav.svelte
+++ web/src/components/Nav.svelte
@@ -8,23 +8,32 @@
             <a
                 class="{segment === 'about'
                     ? 'selected'
-                    : ''} mx-4 font-light text-lg"
+                    : ''} mx-6 font-light text-lg"
                 href="about">About</a
             >
         </li>
-        <li>
+        <li class="w-8">
             <a
-                class="{segment === undefined
-                    ? 'selected'
-                    : ''} mx-4 font-shippori text-2xl"
-                href=".">S / S</a
+                class="{segment === undefined ? 'selected' : ''} text-2xl"
+                href="."
             >
+                <div class="font-shippori text-center">S</div>
+                <div class="relative">
+                    <div
+                        class="absolute inset-0 flex items-center"
+                        aria-hidden="true"
+                    >
+                        <div class="w-full border-t border-black" />
+                    </div>
+                </div>
+                <div class="font-shippori text-center">S</div>
+            </a>
         </li>
         <li>
             <a
                 class="{segment === 'contact'
                     ? 'selected'
-                    : ''} mx-4 font-light text-lg"
+                    : ''} mx-6 font-light text-lg"
                 href="contact">Contact</a
             >
         </li>
