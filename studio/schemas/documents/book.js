3e3f54b20919b48eb6cd4b0f562b30b7cb1c373a
Evan Flynn <erflynn21@gmail.com>
2021-03-03 14:56:19 +0800

    adds book schema

---
+++ studio/schemas/documents/book.js
@@ -0,0 +1,44 @@
+export default {
+  name: 'book',
+  type: 'document',
+  title: 'Book',
+  fields: [
+    {
+      name: 'name',
+      type: 'string',
+      title: 'Name'
+    },
+    // {
+    //   name: 'slug',
+    //   type: 'slug',
+    //   title: 'Slug',
+    //   description: 'Some frontends will require a slug to be set to be able to show the person',
+    //   options: {
+    //     source: 'name',
+    //     maxLength: 96
+    //   }
+    // },
+    {
+      name: 'image',
+      type: 'mainImage',
+      title: 'Image'
+    },
+    {
+      name: 'summary',
+      type: 'text',
+      title: 'Summary'
+    },
+    {
+			name: 'url',
+			type: 'url',
+			title: 'URL'
+    }
+  ],
+  // preview: {
+  //   select: {
+  //     title: 'name',
+  //     subtitle: 'slug.current',
+  //     media: 'image'
+  //   }
+  // }
+}
